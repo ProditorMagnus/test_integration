83a36e3d18beeab65df4786d030d731cc4329c37
ProditorMagnus <public.ravana@gmail.com>
2016-03-10 16:19:03 +0200

    push

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-/**
- * Created by Ravana on 10.03.2016.
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("hw");
+        System.out.println("l2");
     }
 }
